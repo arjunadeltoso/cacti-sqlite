e684318c568b746d0d23641ae6b7c92f49996af0
dat <dat@dublino.(none)>
2013-04-20 16:05:57 +0100

    Committing the original cacti to sqlite port by beagle1

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ cacti-sqlite
 ============
 
 Cacti SQLite
+
+The original work was published by beagle1 on Cacti Forums (http://forums.cacti.net/viewtopic.php?f=21&t=48177), all the merit goes to him/her. Finding it difficult to share patches I created this repository. Fork, patch, improve and enjoy.
+
+
+Installation
+------------
+
+sqlite3 /path/to/db < cacti-sqlite.sql
+configure include/config.php with the proper db path and follow the usual cacti configuration.
---
+++ cacti/include/config.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2012 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+/* make sure these values refect your actual database/host/user/password */
+$database_type = "sqlite";
+$database_default="main";
+$database_hostname = "";
+$database_username = "";
+$database_password = "";
+$database_port = "/var/lib/sqlite/db2.db";
+$database_ssl = false;
+
+/*
+   Edit this to point to the default URL of your Cacti install
+   ex: if your cacti install as at http://serverip/cacti/ this
+   would be set to /cacti/
+*/
+//$url_path = "/cacti/";
+$url_path = "/cacti/";
+
+/* Default session name - Session name must contain alpha characters */
+//$cacti_session_name = "Cacti";
+
+?>
